ed11e64fbda67b0b2667c4b7712fffefbfb55f79
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2021-12-20 03:40:16 +0100

    checking index javascript

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -146,7 +146,7 @@ window.onclick = function(event) {
 jQuery(document).ready(function() {
   window.setTimeout(function(){
     $("#navbar").addClass($("#navTag").val());
-    console.log($("#navTag").val()+ " current")
+    // console.log($("#navTag").val()+ " current")
   },3000);
 
 
